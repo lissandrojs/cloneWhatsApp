cc4cd9947d8f92c8dc802c656188ff9b156233f1
Lissandro kenziano <lissandromr@hotmail.com>
2023-07-24 06:40:31 -0300

    estrutura inicial projeto

---
+++ src/Screens/Chat/index.js
@@ -0,0 +1,23 @@
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Chat = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Chat</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Chat 
